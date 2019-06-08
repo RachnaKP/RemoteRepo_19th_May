9f73fe380b63853bf9bede4d15f2b57757eabdfc
RachnaKP <50794599+RachnaKP@users.noreply.github.com>
2019-06-08 15:26:33 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("I'm dancing");
+		System.out.println("I'm studying");
+    }
+}
+// Execute Thru Jenkins
+//1. Go to configuration page of your job/project
+//2. Select "Execute Windows batch command" from "Add build step" combo
+//3. Type the following on Command field: javac Hello.java java Hello
+//4 Save configuration
+//5 Keep Hello.java on the folder /Jenkins/workspace/
+//6. Build the project/job by clicking "Build Now" link and see the Console Output
+
+//http://paxcel.net/blog/automation-of-warear-deployment-using-jenkins/
