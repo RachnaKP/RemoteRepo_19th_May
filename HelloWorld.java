cfe54df637055d70325fcf9676a274b562e355b4
RachnaKP <50794599+RachnaKP@users.noreply.github.com>
2019-06-08 15:48:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("I'm dancing");
-		System.out.println("I'm studying");
+		System.out.println("I'm studying devops ");
     }
 }
 // Execute Thru Jenkins
